503aac9aefff61f8617be3933fd8cce395d03a34
Sophie Cosgrove <sophie.cosgrovee@hotmail.co.uk>
2020-06-07 20:41:38 +0000

    added sudo

--- script/swarmstack.sh
+++ script/swarmstack.sh
@@ -2,4 +2,4 @@
 . ~/.bashrc
 export DATABASE_URI=${DATABASE_URI}
 export SECRET_KEY=${SECRET_KEY}
-docker stack deploy --compose-file docker-compose.yml sfia2stack
+sudo docker stack deploy --compose-file docker-compose.yml sfia2stack
