0901fc9920be18b865c964d8f5cc31cc4a346f83
Sophie Cosgrove <sophie.cosgrovee@hotmail.co.uk>
2020-06-07 18:56:13 +0000

    added database page

--- service1/application/routes.py
+++ service1/application/routes.py
@@ -1,12 +1,23 @@
-from application import app
+from application import app, db
 from flask import render_template, request, jsonify, Response
+from application.models import Fortunes
 import requests
-import random
 import os
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET', 'POST'])
 def home():
     response = requests.get('http://service4:5003/randomfortune')
     sentence = response.text
+    fortuneData = Fortunes(
+        fortune = sentence
+        )
+    db.session.add(fortuneData)
+    db.session.commit()
+
     return render_template('home.html', sentence = sentence, title = 'Home')
+
+@app.route('/fortunes', methods=['GET'])
+def fortunes():
+    fortuneData = Fortunes.query.all()
+    return render_template('fortunes.html', fortunes = fortuneData)
 
