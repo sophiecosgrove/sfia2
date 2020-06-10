a69fed905c3b7c2f8b3806b66c2ef6e188df676c
Sophie Cosgrove <sophie.cosgrovee@hotmail.co.uk>
2020-06-10 14:11:19 +0000

    resolved nginx

--- service2/application/routes.py
+++ service2/application/routes.py
@@ -5,6 +5,11 @@ import random
 @app.route('/randomtime', methods=['GET'])
 def time():
 
-	list = ['Next week','Next month','Tomorrow','Next weekend','In the next few days','Next year','This weekend']
-	
-	return list[random.randrange(6)]
+    number = random.randint(1,2)
+    if number == 1:
+        time = ['Next week','Tomorrow','Next weekend','In the next few days','This weekend', 'This month', 'The day after tomorrow',
+        'This tuesday', 'This friday', 'This saturday', 'This sunday', 'Next friday']
+    elif number == 2:
+        time = ['Next month', 'In the next few months', 'Next year', 'In the next 3 years', 'This summer', 'This winter',
+        'This autumn', 'This spring', 'Next summer', 'Next winter', 'Next autumn', 'Next spring']
+    return time[random.randrange(12)]
