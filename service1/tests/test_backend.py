a104f3c20c39b00dcbdb98cdd86da6991942095a
Sophie Cosgrove <sophie.cosgrovee@hotmail.co.uk>
2020-06-11 08:51:11 +0000

    mock testing

--- service1/tests/test_backend.py
+++ service1/tests/test_backend.py
@@ -1,4 +1,7 @@
 import unittest
+from unittest.mock import Mock
+from unittest.mock import patch
+import requests
 
 from flask import url_for
 from flask_testing import TestCase
@@ -6,3 +9,40 @@ from flask_testing import TestCase
 from application import app, db
 from application.models import Fortunes
 from os import getenv
+
+class TestBase(TestCase):
+    def create_app(self):
+        config_name = 'testing'
+        app.config.update(SQLALCHEMY_DATABASE_URI=getenv('TEST_DB_URI'),
+            SECRET_KEY=getenv('TEST_SECRET_KEY'),
+            WTF_CSRF_ENABLED=False,
+            DEBUG=True
+            )
+        return app
+
+    def setUp(self):
+        db.session.commit()
+        db.drop_all()
+        db.create_all()
+
+        fortune = Fortunes(fortune = 'Next month you will recieve some good news')
+
+
+        db.session.add(fortune)
+        db.session.commit()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+
+class TestModels(TestBase):
+    def test_repr(self):
+        fortune = Fortunes(fortune = 'Next month you will recieve some good news')
+        print(repr(fortune))
+
+class TestRoutes(TestBase):
+    def test_home(self):
+        with patch.object(requests, 'get') as get_mock:
+            get_mock.return_value = mock_response = Mock()
+            mock_response.status_code = 200
+            assert get_mock() == 200
--- service3/application/routes.py
+++ service3/application/routes.py
@@ -6,11 +6,11 @@ import random
 def phrase():
     number = random.randint(0,2)
     if number == 1 or number == 0:
-        good = ['you will meet your soul mate','a close friend will turn into a lover', 'an important person will step into your life'
+        fortune = ['you will meet your soul mate','a close friend will turn into a lover', 'an important person will step into your life'
         , 'you will gain a lifelong friend', 'a long-lost relative will return', 'a long-lost friend will return',
         'you will recieve great news','you will travel to Europe','a door will close and another one will open', 'your prayers will be answered']
-        return good[random.randrange(10)]
+        return fortune[random.randrange(10)]
     elif number ==2:
-        bad = ['you will recieve bad news','you will face your worst fear', 'you will face great uncertainty',
+        fortune = ['you will recieve bad news','you will face your worst fear', 'you will face great uncertainty',
         'an unwelcome change will occur', 'you will encounter your biggest challenge yet',]
-        return bad[random.randrange(5)]
+        return fortune[random.randrange(5)]
