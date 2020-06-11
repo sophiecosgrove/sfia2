8704b719b29aadc6b85de24f19df76c1fa14ed18
Sophie Cosgrove <sophie.cosgrovee@hotmail.co.uk>
2020-06-11 15:49:57 +0000

    cd into directory

--- script/swarmstack.sh
+++ script/swarmstack.sh
@@ -17,7 +17,5 @@ docker push sophiec0s/service4:latest
 cd ../nginx
 docker build -t sophiec0s/service4:latest .
 docker push sophiec0s/service4:latest
-
+cd ..
 env DATABASE_URI="${DATABASE_URI}" env TEST_DB_URI="${TEST_DB_URI}" docker stack deploy --compose-file docker-compose.yml sfia2stack
-
-echo ${DATABASE_URI} 
