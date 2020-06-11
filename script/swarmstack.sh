b34a849d0032d1fafccab378e41903b5108fcc47
Sophie Cosgrove <sophie.cosgrovee@hotmail.co.uk>
2020-06-11 15:12:54 +0000

    .

--- script/swarmstack.sh
+++ script/swarmstack.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 . ~/.bashrc
-cp ~/.bashrc 
-env DATABASE_URI= ${DATABASE_URI} docker stack deploy --compose-file docker-compose.yml sfia2stack
+cp ~/.bashrc .
+env DATABASE_URI="${DATABASE_URI}" docker stack deploy --compose-file docker-compose.yml sfia2stack
 
 echo ${DATABASE_URI} 
